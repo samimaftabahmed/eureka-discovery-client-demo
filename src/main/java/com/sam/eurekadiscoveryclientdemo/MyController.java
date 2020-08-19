89c050039f7e91250f319c44a29d450f4b1ee950
Samim Aftab Ahmed <samimaftabahmed@gmail.com>
2020-08-20 00:26:54 +0530

    add requestmapping to controller for integration with cloud gateway

--- src/main/java/com/sam/eurekadiscoveryclientdemo/MyController.java
+++ src/main/java/com/sam/eurekadiscoveryclientdemo/MyController.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@RequestMapping("/sam")
 @RestController
 public class MyController {
 
@@ -28,7 +30,7 @@ public class MyController {
     public String getOtherInstances() {
 
         // same project copied but application name changed to mas-client
-        String url = "http://mas-client/service-instances/";
+        String url = "http://mas-client/mas/service-instances/";
         ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
         return responseEntity.getBody();
     }
